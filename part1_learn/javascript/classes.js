d155fa69403c8f281679f2cea5d33b6488781e3c
Zumh <17211423+Zumh@users.noreply.github.com>
2024-01-05 12:55:09 -0500

    done with js

---
+++ part1_learn/javascript/classes.js
@@ -0,0 +1,18 @@
+class Person {
+
+
+    constructor(name, age){
+        this.name = name
+        this.age = age
+    }
+
+    greet(){
+        console.log('Hello, my name is ' + this.name)
+    }
+}
+
+const adam = new Person('Adam', 35)
+adam.greet()
+
+const janja = new Person("Janja", 25)
+janja.greet()
